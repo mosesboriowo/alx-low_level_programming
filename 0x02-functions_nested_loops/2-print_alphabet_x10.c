fab6fe74e0ce631ffbb9bd5e5d766764635a7563
mosesboriowo <boriowomoses2@gmail.com>
2022-10-18 13:37:32 -0700

    new one

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,9 +11,8 @@ void print_alphabet_x10(void);
 
 	while (count++ <= 9)
 	{
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-	_putchar('\n');
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
 	}
 }
