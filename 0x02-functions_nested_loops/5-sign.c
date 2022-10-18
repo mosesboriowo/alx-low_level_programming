8ccc91f05015d6bacbfa0b27a9ace6a26ab7c731
mosesboriowo <boriowomoses2@gmail.com>
2022-10-18 16:00:49 -0700

    moses

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - print the sign of a num
+ * @n: the num we are checking
+ * Return: 1 if the number is greater than 0
+ * 0 if the num is 0
+ * -1 if the num is less than 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
