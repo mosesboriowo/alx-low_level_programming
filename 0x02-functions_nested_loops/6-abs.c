7b2dfe4292124a62ad8c7ead60e60af772815c00
mosesboriowo <boriowomoses2@gmail.com>
2022-10-18 16:09:32 -0700

    no 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - compute abs of a integer
+ * @n: is the num we are checking for remainder
+ *
+ * Return: the abs of the int.
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
